737fabe7529c455aa6c1680b9126e906a290e31f
daviddecoding <daviddecoding@gmail.com>
2017-04-18 11:59:57 -0400

    Added fix.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,12 +3,22 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.concurrent.TimeUnit
 
-import org.openqa.selenium.{By, WebDriver}
+import org.openqa.selenium.{By, WebDriver, WebElement}
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.support.ui.Select
 
 import collection.JavaConverters._
 import java.util.Properties
+
+import Protocols.{TimeRange, TimeRangeWithOption}
+
+object Protocols {
+  case class TimeRange(startDate: Date, endDate: Date)
+  case class TimeRangeWithOption(timeRange: TimeRange, option: WebElement) extends Ordered[TimeRangeWithOption] {
+    override def compare(that: TimeRangeWithOption): Int =
+      timeRange.startDate.compareTo(that.timeRange.startDate)
+  }
+}
 
 object Main extends App {
 
@@ -37,7 +47,7 @@ object Main extends App {
     .asScala
     .zipWithIndex
     .foreach {
-      case (elem, idx) if idx == 3 =>  elem.click()
+      case (elem, idx) if idx == 3 => elem.click()
       case _ =>
     }
 
@@ -45,31 +55,48 @@ object Main extends App {
 
 
   val simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd")
-  val latestPeriodSelect = new Select(driver.findElement(By.id("N32"))).getFirstSelectedOption
-
-  val latestPeriodSelectValue = latestPeriodSelect.getAttribute("value").split("\\|")
-  val latestPeriodSelectText = latestPeriodSelect.getText
-  val startPeriod = simpleDateFormat.parse(latestPeriodSelectValue(0))
-  val endPeriod = simpleDateFormat.parse(latestPeriodSelectValue(1))
+  val periodSelect = new Select(driver.findElement(By.id("N32")))
   val now = new Date()
-  if (startPeriod.before(now) && latestPeriodSelectText.contains("~"))
-  {
-    driver.findElement(By.id("A241N1display")).sendKeys("WWC2017.007 - Core (2017)")
-    driver.findElement(By.id("A251N1display")).sendKeys("30 - Build & QA")
-    driver.findElement(By.id("A261N1display")).sendKeys("Labor")
+  val timeRangeOption =
+    periodSelect
+      .getOptions
+      .asScala
+      .map(toRange)
+      .filter(range => range.timeRange.startDate.before(now) && !range.option.getText.contains("~"))
+      .sorted
+      .map(range => range.option)
+      .headOption
 
-    driver.findElement(By.id("B22_1_1")).sendKeys("8")
-    driver.findElement(By.id("B22_1_2")).sendKeys("8")
-    driver.findElement(By.id("B22_1_3")).sendKeys("8")
-    driver.findElement(By.id("B22_1_4")).sendKeys("8")
-    driver.findElement(By.id("B22_1_5")).sendKeys("8")
+  timeRangeOption match {
+    case Some(option) =>
+      driver.findElement(By.id("N32")).sendKeys(option.getText)
+      driver.findElement(By.id("A241N1display")).sendKeys("WWC2017.007 - Core (2017)")
+      driver.findElement(By.id("A251N1display")).sendKeys("30 - Build & QA")
+      driver.findElement(By.id("A261N1display")).sendKeys("Labor")
 
-    driver.findElement(By.id("review_uixr")).click()
+      driver.findElement(By.id("B22_1_1")).sendKeys("8")
+      driver.findElement(By.id("B22_1_2")).sendKeys("8")
+      driver.findElement(By.id("B22_1_3")).sendKeys("8")
+      driver.findElement(By.id("B22_1_4")).sendKeys("8")
+      driver.findElement(By.id("B22_1_5")).sendKeys("8")
+
+      driver.findElement(By.id("review_uixr")).click()
 
     // This will submit it, uncomment after it works.
-//    driver.findElement(By.id("submit_uixr")).click()
+//      driver.findElement(By.id("submit_uixr")).click()
+
+    case None =>
+      println("Nothing to submit!")
   }
 
 // This will close the driver.
 //  driver.close()
+
+  def toRange(option: WebElement): TimeRangeWithOption = {
+    val latestPeriodSelectValue = option.getAttribute("value")
+    val latestPeriodSelectValueRange = latestPeriodSelectValue.split("\\|")
+    val timeRange = TimeRange(simpleDateFormat.parse(latestPeriodSelectValueRange(0)), simpleDateFormat.parse(latestPeriodSelectValueRange(1)))
+
+    TimeRangeWithOption(timeRange, option)
+  }
 }
